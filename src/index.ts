12a7b02b1aeb5f4d1a58498d27da1cf088a13842
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-06-13 18:26:58 -0300

    feat: cli choose user and download

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import inquirer from "inquirer";
+import Movie from "./interfaces/Movie";
 import User from "./interfaces/User";
 import MovieService from "./services/MovieService";
 
-const user: User = {
-    name: "Nicolas Magno",
-    age: 20,
-    myList: []
-}
+const users: User[] = [
+    {
+        id: 2,
+        name: "Nicolas Magno",
+        age: 20,
+        myList: []
+    }
+]
 
 const questions = [
     {
         type: "input",
         name: "option",
-        message: "Choose an option: \n 1 - Download Movies \n 2 - Rating \n 3 - Exit"
+        message: "Choose an option: \n 1 - Download Movies \n 2 - Rating \n 3 - Show with Average \n 4 - Choose User \n 5 - Exit \n"
     }
 ]
 
@@ -35,8 +39,13 @@ const rateQuestions = [
 const possibleAnswers = {
     DOWNLOAD: '1',
     RATE_MOVIE: '2',
-    EXIT: '3'
+    SHOW_WITH_AVERAGE: '3',
+    CHOOSE_USER: '4',
+    EXIT: '5'
 }
+
+let movies: Movie[];
+let signedUser: number;
 
 async function run() {
     const answers = await inquirer.prompt(questions);
@@ -45,9 +54,38 @@ async function run() {
 
     switch(answers.option) {
         case possibleAnswers.DOWNLOAD:
-                const movies = movieService.listAll();
-                console.log(movies);
+            try {
+                console.log("Downloading movies...")
+                movies = await movieService.listAll();
+                console.log("Downloaded sucessfully.")
+                movies.map(movie => console.log(`${movie.id} - ${movie.name}`))
+            } catch (error) {
+                console.error("Error while downloading :c")
+            } finally {
+                run()
+            }
+
             break;
+        case possibleAnswers.CHOOSE_USER:
+            const whichUser = [
+                {
+                    type: "number",
+                    name: "option",
+                    message: "Type the user ID:"
+                }
+            ]
+
+            users.map(user => console.log(`${user.id} - ${user.name}`))
+            const answers = await inquirer.prompt(whichUser)
+
+            if(answers.option) {
+                signedUser = users.findIndex(user => user.id === answers.option)
+                console.log(signedUser)
+            }
+
+            run()
+            break
+
         case possibleAnswers.RATE_MOVIE:
             let movieId;
             let rate;
--- src/interfaces/Movie.ts
+++ src/interfaces/Movie.ts
@@ -8,9 +8,6 @@ interface Movie {
     ratings: number[];
 
     directedBy: string;
-
-    indicativeRating: IndicativeRating;
-
 }
 
 export default Movie
