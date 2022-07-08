67d63532b17af367219fd69f6500656af013b8af
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-07-08 19:48:56 -0300

    feat: new method added

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const http = require("http");
 const Store = require("./controllers");
 
+const contentType = {
+  "Content-Type": "application/json",
+};
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
-  if (req.url === "/home" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    const products = await new Store().getProduct();
+  if (req.url === "/products" && req.method === "GET") {
+    res.writeHead(200, contentType);
+    const products = await new Store().getProducts();
     res.end(JSON.stringify(products));
+  } else if (req.url.match(/\/products\/([0-9]+)/) && req.method === "GET") {
+    try {
+      const id = req.url.split("/")[2];
+      const product = await new Store().getProduct(id);
+      res.writeHead(200, contentType);
+      res.end(JSON.stringify(product));
+    } catch (error) {
+      res.writeHead(404, contentType);
+      res.end(JSON.stringify({ message: error }));
+    }
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
+    res.writeHead(404, contentType);
     res.end(JSON.stringify({ message: "Route not found." }));
   }
 });
--- controllers.js
+++ controllers.js
@@ -1,9 +1,20 @@
 const data = require("./data");
 
 class StoreController {
-  async getProduct() {
+  async getProducts() {
     return new Promise((resolve, _) => resolve(data));
+  }
+
+  async getProduct(id) {
+    return new Promise((resolve, reject) => {
+      const product = data.find((product) => product.id === parseInt(id));
+      if (product) {
+        resolve(product);
+      } else {
+        reject(`A product with ID ${id} was not found.`);
+      }
+    });
   }
 }
 
-module.exports = StoreController
+module.exports = StoreController;
