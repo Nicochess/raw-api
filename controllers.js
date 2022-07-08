98c4476112cd6ef759f8a82728faee5e8df4ca07
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-07-08 20:04:28 -0300

    feat: new controller method

--- controllers.js
+++ controllers.js
@@ -8,10 +8,24 @@ class StoreController {
   async getProduct(id) {
     return new Promise((resolve, reject) => {
       const product = data.find((product) => product.id === parseInt(id));
-      if (product) {
+
+      if (!product) {
         resolve(product);
       } else {
         reject(`A product with ID ${id} was not found.`);
+      }
+    });
+  }
+
+  async soldProduct(id) {
+    return new Promise((resolve, reject) => {
+      const product = data.find((product) => product.id === parseInt(id));
+
+      if (!product) {
+        reject(`There is no product with ID ${id}`);
+      } else {
+        product.stock -= 1;
+        resolve(product);
       }
     });
   }
